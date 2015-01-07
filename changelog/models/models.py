94ae31cf27ddfd1f213ac10e21e36255fda4cd90
Peter Grace <pete.grace@gmail.com>
2015-01-07 03:50:19 +0000

    Setup some models

--- changelog/models/models.py
+++ changelog/models/models.py
@@ -3,6 +3,7 @@ from sqlalchemy import (
     Index,
     Integer,
     Text,
+    DateTime
     )
 
 from sqlalchemy.ext.declarative import declarative_base
@@ -17,15 +18,70 @@ from zope.sqlalchemy import ZopeTransactionExtension
 DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
 Base = declarative_base()
 
-
-class MyModel(Base):
-    __tablename__ = 'models'
+class SiteModel(Base):
+    __tablename__ = 'Sites'
     id = Column(Integer, primary_key=True)
-    name = Column(Text)
-    value = Column(Integer)
+    Name = Column(Text)
+    
+    def __init__(self, name):
+        self.Name = name
 
-    def __init__(self, name, value):
-        self.name = name
-        self.value = value
+class SystemModel(Base):
+    __tablename__ = 'Systems'
+    id = Column(Integer, primary_key=True)
+    Name = Column(Text)
 
-Index('my_index', MyModel.name, unique=True, mysql_length=255)
+    def __init__(self, name):
+        self.Name = name
+
+class UserModel(Base):
+    __tablename__ = 'Users'
+    id = Column(Integer, primary_key=True)
+    email = Column(Text)
+
+class SystemAssociationModel(Base):
+    __tablename__ = 'SystemAssociation'
+    id = Column(Integer, primary_key=True)
+    Entry = Column(Integer, ForeignKey("ChangelogEntry.id")
+    System = Column(Integer,ForeignKey("Systems.id"))
+
+    def __init__(self, entry, system):
+        self.Entry = entry
+        self.System = system
+
+class SiteAssociationModel(Base):
+    __tablename__ = 'SiteAssociation'
+    id = Column(Integer, primary_key=True)
+    Entry = Column(Integer, ForeignKey("ChangelogEntry.id")
+    Site = Column(Integer,ForeignKey("Sites.id"))
+
+    def __init__(self, entry, site):
+        self.Entry = entry
+        self.Site = site
+
+
+class ChangeLogEntryModel(Base):
+    __tablename__ = 'ChangelogEntry'
+    id = Column(Integer, primary_key=True)
+    ParentList = Column(Integer, ForeignKey("ChangeLogLists.id")
+    User = Column(Integer, ForeignKey("Users.id"))
+    DateCreated = Column(DateTime)
+    DateEdited = Column(DateTime)
+    Change = Column(Text)
+
+    def __init__(self, user, system, site, create, edit, change):
+        self.User = user
+        self.DateCreated = create
+        self.DateEdited = edit
+        self.Change = change
+
+
+class ChangeLogListModel(Base):
+    __tablename__ = 'ChangeLogLists'
+    id = Column(Integer, primary_key=True)
+    Name = Column(Text)
+    User = Column(Integer, ForeignKey("Users.id"))
+
+    def __init__(self, name, user):
+        self.User=user
+        self.Name=name
